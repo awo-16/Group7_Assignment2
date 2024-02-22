8e99fea7347a04b4c765a56d8917a757d098b65d
Elson-Ricafrente <ricafrentee@student.douglascollege.ca>
2024-02-21 19:25:17 -0800

    feature: Created Model_eri_77 class

---
+++ Group7_Assignment2/src/main/java/com/csis3275/model/Model_eri_77.java
@@ -0,0 +1,29 @@
+package com.csis3275.model;
+
+public class Model_eri_77 {
+
+	private String imageName;
+	private String description;
+
+	public Model_eri_77(String imageName, String description) {
+		this.imageName = imageName;
+		this.description = description;
+	}
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
